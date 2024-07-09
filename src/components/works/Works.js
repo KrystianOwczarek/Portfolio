d3701be8ec1eac30b15ed45a4b5887497a3e52f2
Krystian Owczarek <krystianowczarek@laptop-Krystian.local>
2024-07-09 20:30:27 +0200

    update

--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -9,8 +9,8 @@ import { useTranslation } from 'react-i18next';
 import './Works.css';
 
 // Import ../../assets/recentprojects/
-import Configurator from '../../assets/recentprojects/hal-3D-konfigurator.svg';
-import DDD from '../../assets/recentprojects/dzien-dobry-doktorze.svg';
+import BarberShop from '../../assets/recentprojects/barber-svg.svg';
+import BarberPlanner from '../../assets/recentprojects/barber-planner-svg.svg';
 import Ecris from '../../assets/recentprojects/ecris.svg';
 import Amazon from '../../assets/recentprojects/amazon.svg';
 
@@ -28,36 +28,52 @@ export const Works = () => {
   const [projects, setProjects] = useState([
     { 
       id: 1,
-      title: 'Hal 3D Configurator', 
-      description: `The Hal 3D Configurator project is an advanced web application designed to provide users with an immersive experience of customizing virtual spaces. Built upon the powerful Three.js framework, it allows users to interactively design and personalize various aspects of halls.`,
-      alter: t('Hal 3D Configurator'),
-      image: `${Configurator}`,
-      url: 'https://konfigurator-hal-sk-system.vercel.app'
+      title: 'Barber Shop', 
+      description: `The website created for a barber.`,
+      alter: t('Barber Shop'),
+      image: `${BarberShop}`,
+      url: 'https://barbershopraszkow.pl'
     },
     { 
       id: 2,
-      title: 'Good Morning Doctor', 
-      description: `This application is used by pharmaceutical company representatives to schedule visits to doctors. It offers numerous functionalities, including setting vacations, determining the number of visits per day, specifying days without representatives, and, on the representatives' side, options such as notifying doctors via SMS upon the representative's arrival, adding selected doctors from the database, among others. The application also includes an admin panel, from which messages can be sent to doctors and representatives via the built-in mailbox within the application, as well as managing accounts.`,
-      alter: 'Dzień Dobry Doktorze',
-      image: `${DDD}`,
-      url: 'https://aplikacja.dziendobrydoktorze.pl'
+      title: 'Barber Planner', 
+      description: `Barber Planner is a web application designed for easy and convenient appointment scheduling. After registering as a client, you gain access to the planner, where you can book appointments with your barber. The app allows you to choose services, book appointments up to 3 months in advance, cancel appointments, and access a list of your reserved slots. Additionally, the administrator account provides the capability to book appointments for clients who schedule by phone. Entering the client's phone number assigns the appointment to their account. The administrator can also add new services and manage the list of appointments in a simple and efficient manner.`,
+      alter: t('Barber Planner'),
+      image: `${BarberPlanner}`,
+      url: 'https://barber-planer.web.app'
     },
-    { 
-      id: 3,
-      title: 'Ecris', 
-      description: `The application provides services related to information on criminal records in Europe. It allows access to information about crimes, court sentences, or other aspects related to the criminal history of individuals in various European countries.`,
-      alter: 'Ecris',
-      image: `${Ecris}`,
-      url: 'https://ecris-beta.onrender.com/'
-    },
-    { 
-      id: 4,
-      title: 'Inbound appointment Slam Oschatz', 
-      description: `A website portfolio project for the Startup Dev Team
-      built using MEVN stack to demonstrate the CRUD capabilities of the tech stack.`,
-      alter: 'Inbound appointment Slam Oschatz',
-      image: `${Amazon}`,
-    },
+    // { 
+    //   id: 1,
+    //   title: 'Hal 3D Configurator', 
+    //   description: `The Hal 3D Configurator project is an advanced web application designed to provide users with an immersive experience of customizing virtual spaces. Built upon the powerful Three.js framework, it allows users to interactively design and personalize various aspects of halls.`,
+    //   alter: t('Hal 3D Configurator'),
+    //   image: `${Configurator}`,
+    //   url: 'https://konfigurator-hal-sk-system.vercel.app'
+    // },
+    // { 
+    //   id: 2,
+    //   title: 'Good Morning Doctor', 
+    //   description: `This application is used by pharmaceutical company representatives to schedule visits to doctors. It offers numerous functionalities, including setting vacations, determining the number of visits per day, specifying days without representatives, and, on the representatives' side, options such as notifying doctors via SMS upon the representative's arrival, adding selected doctors from the database, among others. The application also includes an admin panel, from which messages can be sent to doctors and representatives via the built-in mailbox within the application, as well as managing accounts.`,
+    //   alter: 'Dzień Dobry Doktorze',
+    //   image: `${DDD}`,
+    //   url: 'https://aplikacja.dziendobrydoktorze.pl'
+    // },
+    // { 
+    //   id: 3,
+    //   title: 'Ecris', 
+    //   description: `The application provides services related to information on criminal records in Europe. It allows access to information about crimes, court sentences, or other aspects related to the criminal history of individuals in various European countries.`,
+    //   alter: 'Ecris',
+    //   image: `${Ecris}`,
+    //   url: 'https://ecris-beta.onrender.com/'
+    // },
+    // { 
+    //   id: 4,
+    //   title: 'Inbound appointment Slam Oschatz', 
+    //   description: `A website portfolio project for the Startup Dev Team
+    //   built using MEVN stack to demonstrate the CRUD capabilities of the tech stack.`,
+    //   alter: 'Inbound appointment Slam Oschatz',
+    //   image: `${Amazon}`,
+    // },
   ]);
 
   return (
